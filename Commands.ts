b6a1a7935c97b05249920009d5c809f508935d33
rafaballerini <rafaella_ballerini@hotmail.com>
2021-04-27 17:12:41 -0300

    Desconto da Alura

--- Commands.ts
+++ Commands.ts
@@ -3,14 +3,14 @@ interface ICommands{
 }
 
 export const comandos : ICommands = {
-    '!redes' : 'Redes sociais da Rafa: https://beacons.ai/rafaballerini',
-    '!discord' : 'Faça parte da nossa comunidade: https://discord.gg/pzRrsEgHpn',
-    '!instagram' : 'Siga a Rafa no instagram: https://instagram.com/rafaballerini',
-    '!youtube' : 'Se inscreva no canal da Rafa: https://youtube.com/RafaellaBallerini',
-    '!twitter' : 'Siga a Rafa no twitter: https://twitter.com/ballerinirafa',
-    '!github' : 'Veja o github da Rafa: https://github.com/rafaballerini',
-    '!org' : 'Nossa organização no Github: https://github.com/Ballerini-Server',
-    '!rock' : 'Ouça a playlist de Rock da Rafa: https://open.spotify.com/embed/playlist/5TUxgTIxzLbLVh7RUf9V8i',
-    '!playlist' : 'Ouça a playlist criada pela comunidade da Rafa: https://open.spotify.com/playlist/2sD801KTeEZb0wNi2k8rzc?si=M8Ww1fHaR8u0q0vZQv6FOQ',
-    '!alura': 'Conheça a Alura: https://www.alura.com.br/',
+    '<3redes' : 'Redes sociais da Rafa: https://beacons.ai/rafaballerini',
+    '<3discord' : 'Faça parte da nossa comunidade: https://discord.gg/pzRrsEgHpn',
+    '<3instagram' : 'Siga a Rafa no instagram: https://instagram.com/rafaballerini',
+    '<3youtube' : 'Se inscreva no canal da Rafa: https://youtube.com/RafaellaBallerini',
+    '<3twitter' : 'Siga a Rafa no twitter: https://twitter.com/ballerinirafa',
+    '<3github' : 'Veja o github da Rafa: https://github.com/rafaballerini',
+    '<3org' : 'Nossa organização no Github: https://github.com/Ballerini-Server',
+    '<3rock' : 'Ouça a playlist de Rock da Rafa: https://open.spotify.com/embed/playlist/5TUxgTIxzLbLVh7RUf9V8i',
+    '<3playlist' : 'Ouça a playlist criada pela comunidade da Rafa: https://open.spotify.com/playlist/2sD801KTeEZb0wNi2k8rzc?si=M8Ww1fHaR8u0q0vZQv6FOQ',
+    '<3alura': 'Desconto na Alura: https://www.alura.com.br/promocao/rafaballerini',
 }
